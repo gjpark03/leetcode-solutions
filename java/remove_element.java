a207eba14e4ee4deb2ce5556088fcf5e6b9b4af1
Grace Park <gjpark@usc.edu>
2024-07-30 10:17:13 -0700

    Solved remove_element.java

---
+++ java/remove_element.java
@@ -0,0 +1,21 @@
+/*
+Given an integer array nums and an integer val, remove all occurrences of val in nums in-place. The order of the elements 
+may be changed. Then return the number of elements in nums which are not equal to val.
+Consider the number of elements in nums which are not equal to val be k, to get accepted, you need to do the following things:
+Change the array nums such that the first k elements of nums contain the elements which are not equal to val. 
+The remaining elements of nums are not important as well as the size of nums.
+Return k.
+*/
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int sol = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != val) {
+                nums[sol] = nums[i];
+                sol++;
+            }
+        }
+        return sol;
+    }
+}
