41b7bcb6ceab71553c1e3876a12b63745f85b387
Grace Park <gjpark@usc.edu>
2024-07-30 13:14:16 -0700

    Solved count-primes.java

---
+++ java/count-primes.java
@@ -0,0 +1,33 @@
+/*
+Given an integer n, return the number of prime numbers that are strictly less than n.
+*/
+
+class Solution {
+    public int countPrimes(int n) {
+        if(n <= 2) {
+            return 0;
+        }
+
+        boolean[] isPrime = new boolean[n];
+        for (int i = 2; i < n; i++) {
+            isPrime[i] = true;
+        }
+
+        // Sieve of Eratosthenes algorithm
+        for (int i = 2; i * i < n; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j < n; j += i) {
+                    isPrime[j] = false;
+                }
+            }
+        }
+
+        int count = 0;
+        for(int i = 2; i < n; i++) {
+            if(isPrime[i]) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
