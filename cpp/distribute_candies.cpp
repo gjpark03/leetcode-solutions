0f8d33200ee87178fd3547ae01350e31c45edb38
Grace Park <gjpark@usc.edu>
2024-11-15 01:29:32 -0800

    solved distribute_candies.cpp

---
+++ cpp/distribute_candies.cpp
@@ -0,0 +1,19 @@
+/*
+  Alice has n candies, where the ith candy is of type candyType[i]. 
+  Alice noticed that she started to gain weight, so she visited a doctor.
+  The doctor advised Alice to only eat n / 2 of the candies she has (n is always even). 
+  Alice likes her candies very much, and she wants to eat the maximum number of different 
+  types of candies while still following the doctor's advice. Given the integer array candyType 
+  of length n, return the maximum number of different types of candies she can eat if she only eats n / 2 of them.
+*/
+
+class Solution {
+public:
+    int distributeCandies(vector<int>& candyType) {
+        unordered_map<int, int> typeCount;
+        for(int i = 0; i < candyType.size(); i++) {
+            typeCount[candyType[i]]++;
+        }
+        return min((int)typeCount.size(), (int)(candyType.size() / 2));
+    }
+};
