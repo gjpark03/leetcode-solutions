2bf364b1d3a40769497504c2c0b3e983c6cfbe48
Grace Park <parkjgrace2025@gmail.com>
2025-08-01 22:44:29 -0700

    Create majority-element.py

---
+++ python/majority-element.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        candidate = None
+        count = 0
+        for num in nums:
+            if count == 0:
+                candidate = num
+                count = 1
+            elif num == candidate:
+                count += 1
+            else:
+                count -= 1
+
+        return candidate
+
+
