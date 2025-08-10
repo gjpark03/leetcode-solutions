f77371cb14a388cb6eaebd493d61a4cc4c2aa177
Grace Park <parkjgrace2025@gmail.com>
2025-08-10 00:08:33 -0700

    Create binary-search.py

---
+++ python/binary-search.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            middle = (left + right) // 2
+            if nums[middle] == target:
+                return middle
+            elif nums[middle] < target:
+                left = middle + 1
+            else:
+                right = middle - 1
+        return -1
+        
